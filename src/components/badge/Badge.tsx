6b9083b9a7ae3a45761fefbf724c99e508b040f2
linli tian <linli.tian@arcticwolf.com>
2020-12-10 17:04:28 -0500

    update the Button using the emotion.

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
 import { useTheme } from '../../hooks';
 import { GlobalTheme } from '../..';
 
